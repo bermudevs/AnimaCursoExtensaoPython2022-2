ee4e1a720eaa5337fc61535f70e459675cfb126d
Bermudes <111182710+bermudevs@users.noreply.github.com>
2022-10-20 01:25:26 +0000

    Diversas formas de saída no console

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ com três aspas simples
 
 print("Hello world!")
 
-funcao = "Pedreiro"
-qtd = 24
+nome = "Fúvio"
+idade = 24
+frase = "Minha idade é"
 
-print(funcao, qtd)
+print(nome, idade)
+
+print("Minha idade é "+str(idade))
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}".format(idade))
+
+print("Meu nome é {} e minha idade é {} anos" .format (nome,idade))
