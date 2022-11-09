c71a17da4dcbbf706c746329603563a04a9088cd
Bermudes <111182710+bermudevs@users.noreply.github.com>
2022-11-09 20:32:39 +0000

    Comando de entrada input

---
+++ aula_1_2022_10_19.py
@@ -0,0 +1,21 @@
+# My first Python project!
+#comentário em linha usa asteríscos
+#print() = comando de saída
+'''
+comentário em blocos
+com três aspas simples
+'''
+
+print("Hello world!")
+
+nome = "Fúvio"
+idade = 24
+frase = "Minha idade é"
+
+print(nome, idade)
+
+print("Minha idade é "+str(idade))
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}".format(idade))
+
+print("Meu nome é {} e minha idade é {} anos" .format (nome,idade))
--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-# My first Python project!
-#comentário em linha usa asteríscos
-#print() = comando de saída
+# comando input(): permissão usuário digite algo
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+# comando de saída... exibir na tela
+print("Seja bem vindo, {}! Sua idade é {} anos".format (nome,idade))
+
+# mostrar o dobro da idade
 '''
-comentário em blocos
-com três aspas simples
-'''
+poderia colocar em uma variável o dobro
+mas fiz direto no print
+exemplo
+dobro = idade * 2
+tive que converter a idade em número inteiro
+lá na atribuição da variável
+'''  
+print("Daqui {} anos, você terá {} anos".format (idade,idade*2))
 
-print("Hello world!")
-
-nome = "Fúvio"
-idade = 24
-frase = "Minha idade é"
-
-print(nome, idade)
-
-print("Minha idade é "+str(idade))
-print(f"Minha idade é {idade}\n")
-print("Minha idade é {}".format(idade))
-
-print("Meu nome é {} e minha idade é {} anos" .format (nome,idade))
