1a8c4f43e07b07cbe85f43418902930d332c5947
Bermudes <111182710+bermudevs@users.noreply.github.com>
2022-11-21 18:47:24 +0000

    exemplo de while e lista

--- aula_3_2022_11_9a.py
+++ aula_3_2022_11_9a.py
@@ -1,4 +1,4 @@
-# neregressiva
+# regressiva
 contador = 10
 
 while(contador >= 1):
@@ -12,3 +12,41 @@ contador = 1
 while(contador <= 10):
   print(contador)
   contador = contador+1
+
+#laço for (python for = for each)
+fruta = "morango"
+print(fruta)
+
+fruta1 = "morango"
+fruta2 = "laranja"
+fruta3 = "pêra"
+
+#lista
+frutas = ["morango", "laranja", "pêra"]
+
+#quero exibir apenas a 3 fruta(pêra)
+print(frutas[1])
+
+#quantas frutas têm na lista
+print(len(frutas))
+
+#quero incluir uma fruta nova
+frutas.append("manga")
+
+print(len(frutas))
+print(frutas[3])
+print("")
+
+
+print("FRUTAS EM REPETIÇÃO")
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+print(frutas[3])
+print("")
+
+print("FRUTAS DENTRO DO LOOP")
+i = 0 #(i de index)
+while(i<len(frutas)):
+  print(frutas[i])
+  i = i + 1
