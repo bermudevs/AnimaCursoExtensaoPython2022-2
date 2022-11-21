e6ce0cc4e6c24e6eae8484de9cf33896d88b0497
Bermudes <111182710+bermudevs@users.noreply.github.com>
2022-11-21 23:01:39 +0000

    dois parâmetros com valor padrao

--- aula_3_2022_11_09b.py
+++ aula_3_2022_11_09b.py
@@ -15,11 +15,31 @@ print(imposto2)
 
 #declaração da função(como funciona)
 def calcular_imposto(preco_item):
-  imposto = preco_item * 0.05
+  imposto = preco_item * 0.07
   return imposto
 
 #Aqui é o uso... aqui é o imposto a calcular...
 preco = 485
 imposto = calcular_imposto(preco) #o retur da linha 19 que manda pra cá a info novamente
-print(imposto)
+print(f"esse aqui é com a função (7%):{imposto}")
 print("")
+
+#agora a alíquota é 7#
+
+valores = [1.99, 22.30, 59.90, 1929.50]
+
+for valor in valores:
+  print(f"o imposto de {valor} é {calcular_imposto(valor)}")
+
+#declarar função calcular_imposto_aliquota que recebe dois parametros
+
+def calcular_imposto_aliquota(valor, aliquota=7):
+  imposto = valor * aliquota / 100
+  return imposto
+
+for valor in valores:
+  print(f"o imposto de {valor} é {calcular_imposto(valor)}")
+
+# imposto a 10%
+for valor in valores:
+  print(f"o imposto de {valor} é {calcular_imposto_aliquota(valor, 10)}")
